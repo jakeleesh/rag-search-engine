cb59db402b782f6e5179e35ef477a674bdc9f9a2
Jake Lee <leeshaoherng@hotmail.com>
2026-02-20 19:26:09 +0800

    feat: Remove all punctuation

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,18 @@
 from lib.search_utils import load_movies
+import string
+
+def clean_text(text):
+    text = text.lower()
+    # Mapp all characters in third argument to None
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
+    query = clean_text(query)
     for movie in movies:
-        if query.lower() in movie['title'].lower():
+        if query in clean_text(movie['title']):
             res.append(movie)
         if len(res) == n_results:
             break
