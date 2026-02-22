8385389eeb0c3ae13fd805954e1d8bfe5892ef6b
Jake Lee <leeshaoherng@hotmail.com>
2026-02-22 20:36:17 +0800

    feat: Stemming: use nltk to reduce to root form

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,8 @@
 from lib.search_utils import load_movies, load_stopwords
 import string
+from nltk.stem import PorterStemmer
+
+stemmer = PorterStemmer()
 
 def clean_text(text):
     text = text.lower()
@@ -19,6 +22,7 @@ def tokenize_text(text):
         return False
     for tok in text.split():
         if _filter(tok):
+            tok = stemmer.stem(tok)
             res.append(tok)
     return res
 
