c39648e9b9fe5310b0dfb7e85411d70dd9ecd303
Jake Lee <leeshaoherng@hotmail.com>
2026-02-22 15:26:35 +0800

    feat: Tokenization to be able to search for partial matches, not just exact substrings

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -7,12 +7,28 @@ def clean_text(text):
     text = text.translate(str.maketrans("", "", string.punctuation))
     return text
 
+def tokenize_text(text):
+    text = clean_text(text)
+    # if tok says not None like empty string
+    tokens = [tok for tok in text.split() if tok]
+    return tokens
+
+def has_matching_token(query_tokens, movie_tokens):
+    """Compare query to document. Given the two, are they a match or not."""
+    # Have every unique combination
+    for query_tok in query_tokens:
+        for movie_tok in movie_tokens:
+            if query_tok in movie_tok:
+                return True
+    return False
+
 def search_command(query, n_results):
     movies = load_movies()
     res = []
-    query = clean_text(query)
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query in clean_text(movie['title']):
+        movie_tokens = tokenize_text(movie['title'])
+        if has_matching_token(query_tokens, movie_tokens):
             res.append(movie)
         if len(res) == n_results:
             break
