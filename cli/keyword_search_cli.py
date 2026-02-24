def0e8ff1d7fc0a7c8b093e3bdd4bbd33c1d58dc
Jake Lee <leeshaoherng@hotmail.com>
2026-02-24 13:29:32 +0800

    feat: Implement InvertedIndex class

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.keyword_search import search_command
+from lib.keyword_search import search_command, build_command
 
 
 def main() -> None:
@@ -10,6 +10,8 @@ def main() -> None:
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+
+    search_parser = subparsers.add_parser("build", help="")
 
     args = parser.parse_args()
 
@@ -20,6 +22,8 @@ def main() -> None:
             results = search_command(args.query, 5)
             for i, result in enumerate(results):
                 print(f"{i} {result['title']}")
+        case "build":
+            build_command()
         case _:
             parser.print_help()
 
